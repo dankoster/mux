ebcf5abfcf476734f7aeaf7e3306ba01ded4e716
Dan Koster <koster.dan@gmail.com>
2024-09-26 14:04:28 -0700

    use simpler map data structures

--- server/api.ts
+++ server/api.ts
@@ -6,20 +6,27 @@ export { api }
 export type ApiRoute = "sse" | "setColor" | "setText"
 export type SSEvent = "pk" | "id" | "connections"
 export type Connection = {
-	id: number,
+	id: string,
 	color?: string,
 	text?: string,
-	update: (key: string, value: string) => void
 }
+
+const sseEvent: { [Property in SSEvent]: Property} = {
+  pk: "pk",
+  id: "id",
+  connections: "connections"
+}
+
+const AUTH_TOKEN_HEADER_NAME = sseEvent.pk
 
 const apiRoute: { [Property in ApiRoute]: Property } = {
 	sse: "sse",
 	setColor: "setColor",
 	setText: "setText"
-};
+}
 
-const connections: Array<Connection> = []
-const privateKeys: { [key: string]: string } = {}
+const connectionByUUID = new Map<string, Connection>()
+const updateFunctionByUUID = new Map<string, (value: string) => void>()
 
 function sseMessage(event: SSEvent, data?: string, id?: string) {
 	//https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#event_stream_format
@@ -30,76 +37,54 @@ function sseMessage(event: SSEvent, data?: string, id?: string) {
 	lines.push('\r\n');
 	const msg = lines.join('\r\n');
 	return new TextEncoder().encode(msg);
-}	
+}
 
 function notifyAllConnections() {
+	const connections = Array.from(connectionByUUID.values())
 	const value = JSON.stringify(connections)
-	connections.forEach(con => con.update(apiRoute.sse, value))
+	updateFunctionByUUID.forEach(update => update(value))
 }
 
-function getId(pk: string) {
-	const str = Object.keys(privateKeys).find(id => privateKeys[id] === pk)
-	return str && Number.parseInt(str)
-}
-
-function getPkHeader(request: Request) {
-	const pk = request.headers.get('pk');
-	if (!pk)
-		throw new Error('missing pk header');
-	return pk;
-}
-
-function getConnectionIndex(id: string | number | undefined) {
-	const index = connections.findIndex(c => c.id === id);
-
-	if (!connections[index]) throw new Error(`${index} not found in connections`);
-	
-	return index;
+function updateConnectionProperty(req: Request, prop: keyof Connection, value: string) {
+	const uuid = req.headers.get(AUTH_TOKEN_HEADER_NAME);
+	if (!uuid) throw new Error(`Missing ${AUTH_TOKEN_HEADER_NAME} header`);
+	const con = connectionByUUID.get(uuid);
+	if (!con) throw new Error(`No connection found for key ${uuid}`);
+	con[prop] = value;
+	connectionByUUID.set(uuid, con);
 }
 
 const api = new Router();
 api.post(`/${apiRoute.setText}`, async (context) => {
-	const pk = getPkHeader(context.request);
-	const id = getId(pk);
-	const index = getConnectionIndex(id);
-	connections[index].text = await context.request.body.text();
+	updateConnectionProperty(context.request, "text", await context.request.body.text());
 	context.response.status = 200
 	notifyAllConnections()
 })
 
 api.post(`/${apiRoute.setColor}`, async (context) => {
-	const pk = getPkHeader(context.request);
-	const id = getId(pk);
-	const index = getConnectionIndex(id);
-	connections[index].color = await context.request.body.text();
+	updateConnectionProperty(context.request, "color", await context.request.body.text());
 	context.response.status = 200
 	notifyAllConnections()
 });
 
 //https://deno.com/blog/deploy-streams
 api.get(`/${apiRoute.sse}`, async (context) => {
-	const id = Date.now()
+	const uuid = crypto.randomUUID()
 	context.response.headers.append("Content-Type", "text/event-stream");
 	context.response.body = new ReadableStream({
 		start(controller) {
-			//console.log('SSE connected', context.request, id)
-			if (!connections.some(c => c.id === id)) {
-				connections.push({
-					id,
-					update: (key, value) => controller.enqueue(sseMessage('connections', value))
-				})
-			}
-			privateKeys[id] = crypto.randomUUID()
-			controller.enqueue(sseMessage('id', id.toString()))
-			controller.enqueue(sseMessage('pk', privateKeys[id]))
+			const id = Date.now().toString()
+			const connection = { id }
+			connectionByUUID.set(uuid, connection)
+			updateFunctionByUUID.set(uuid, (value) => controller.enqueue(sseMessage('connections', value)))
+
+			controller.enqueue(sseMessage(sseEvent.id, id.toString()))
+			controller.enqueue(sseMessage(sseEvent.pk, uuid))
 			notifyAllConnections()
 		},
 		cancel() {
-			//console.log('SSE disconnected', context.request.ip, id)
-			delete (privateKeys[id])
-			const index = connections.findIndex(c => id === c.id)
-			if (index >= 0)
-				connections.splice(index, 1)
+			updateFunctionByUUID.delete(uuid)
+			connectionByUUID.delete(uuid)
 			notifyAllConnections()
 		},
 	});
