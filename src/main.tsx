391950df7e6258adef73dcd37ca86d2b000cc93f
Dan Koster <koster.dan@gmail.com>
2024-09-30 14:27:35 -0700

    move connection panel to new file

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,10 @@
 import "./index.css"
 import "./main.css"
 
-import { For, onMount, Show } from "solid-js";
+import { For, Show } from "solid-js";
 import { render } from "solid-js/web";
 import server from "./data";
-import { Connection } from "../server/api";
-
-const ConnectionPanel = (props: { connection: Connection, showControls: boolean }) => {
-
-	let ref: HTMLInputElement
-
-	onMount(() => {
-		ref?.focus()
-	})
-
-	return <div
-		class="connection controls"
-		// classList={{ "controls": props.showControls }}
-		style={{ "background-color": props.connection.color }}>
-		{!props.showControls && <>
-			<h2 textContent={props.connection.text || "🌈 🤔"}></h2>
-			<h2>{props.connection.status === "online" ? "👀" : `😴`}</h2>
-		</>}
-		{props.showControls && <>
-			<input
-				ref={ref}
-				type="text"
-				maxlength="123"
-				placeholder="Leave a message! 👋 🌈 👉"
-				onchange={(e) => server.setText(e.target.value)}
-				onfocus={(e) => e.target.setSelectionRange(0, e.target.value.length)}
-				value={props.connection.text ?? ''} />
-			<input
-				type="color"
-				oninput={(e) => e.target.parentElement.style.backgroundColor = e.target.value }
-				onchange={(e) => server.setColor(e.target.value)}
-				value={props.connection.color} />
-		</>}
-	</div>
-}
+import { ConnectionPanel } from "./ConnectionPanel";
 
 const App = () => {
 	return <>
