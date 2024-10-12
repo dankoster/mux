e9d8b0ed7153045201f76988d89b11c99964b8bc
Dan Koster <koster.dan@gmail.com>
2024-10-11 21:23:10 -0700

    refactor peer connection into a class to support multiple connections

--- src/VideoCall.tsx
+++ src/VideoCall.tsx
@@ -5,6 +5,227 @@ import "./VideoCall.css"
 import server from "./data"
 import { Connection, Room } from "../server/api";
 
+const servers: RTCConfiguration = {
+	iceServers: [
+		// {
+		// 	urls: [
+		// 		'stun:stun1.l.google.com:19302',
+		// 		'stun:stun2.l.google.com:19302'
+		// 	],
+		// },
+		{
+			urls: "stun:stun.relay.metered.ca:80",
+		},
+		{
+			urls: "turn:global.relay.metered.ca:80",
+			username: "20cd52d0dc022700b2755c26",
+			credential: "MNMabfdDEZeLlOFU",
+		},
+		{
+			urls: "turn:global.relay.metered.ca:80?transport=tcp",
+			username: "20cd52d0dc022700b2755c26",
+			credential: "MNMabfdDEZeLlOFU",
+		},
+		{
+			urls: "turn:global.relay.metered.ca:443",
+			username: "20cd52d0dc022700b2755c26",
+			credential: "MNMabfdDEZeLlOFU",
+		},
+		{
+			urls: "turns:global.relay.metered.ca:443?transport=tcp",
+			username: "20cd52d0dc022700b2755c26",
+			credential: "MNMabfdDEZeLlOFU",
+		},
+	],
+	iceCandidatePoolSize: 10,
+};
+
+type pcInit = {
+	polite: boolean,
+	sendMessage: (message: {}) => Promise<Response>,
+	onConnect: (localStream: MediaStream, remoteStream: MediaStream) => void,
+	onDisconnect: () => void
+}
+
+class PeerConnection extends EventTarget {
+	localStream: MediaStream | undefined
+	remoteStream = new MediaStream();
+	loggingAbortControllers: AbortController[] = []
+	localRTCRtpSenders: RTCRtpSender[] = []
+
+	constraints = { audio: true, video: true }
+
+	pc: RTCPeerConnection
+
+	polite = false;
+	makingOffer = false;
+	ignoreOffer = false;
+
+	onConnect: (localStream: MediaStream, remoteStream: MediaStream) => void
+	onDisconnect: () => void
+	sendMessage: (message: {}) => Promise<Response>
+
+	constructor({ polite, onConnect, onDisconnect, sendMessage }: pcInit) {
+		super()
+
+		this.pc = new RTCPeerConnection(servers)
+		this.onConnect = onConnect
+		this.onDisconnect = onDisconnect
+		this.sendMessage = sendMessage
+		this.polite = polite
+
+		// Pull tracks from remote stream, add to video stream
+		this.pc.ontrack = (event) => {
+			const track = event.track
+			console.log(`got ${event.type}: ${track.muted ? "muted" : "un-muted"} ${track.kind} from peer connection`, track.label)
+			track.addEventListener('end', () => this.remoteStream.removeTrack(track))
+			// track.addEventListener('mute', () => this.remoteStream.removeTrack(track))
+			track.addEventListener('unmute', () => this.remoteStream.addTrack(track))
+			//this.remoteStream.addTrack(track);
+			this.logTrackEvents(track, 'remote');
+		};
+
+		this.pc.onnegotiationneeded = async () => {
+			try {
+				this.makingOffer = true;
+				await this.pc.setLocalDescription();
+				//await server.sendDM(otherUser()?.id, JSON.stringify({ description: this.pc.localDescription }))
+				await sendMessage({ description: this.pc.localDescription })
+			} catch (err) {
+				console.error(err);
+			} finally {
+				this.makingOffer = false;
+			}
+		};
+
+		this.pc.oniceconnectionstatechange = () => {
+			console.log('oniceconnectionstatechange', this.pc.iceConnectionState, this.pc.signalingState)
+			if (this.pc.iceConnectionState === "failed") {
+				this.pc.restartIce();
+			}
+
+			if (this.pc.iceConnectionState === 'disconnected') {
+				onDisconnect()
+				console.log(this.remoteStream)
+			}
+		};
+
+		// this.pc.onicecandidate = ({ candidate }) => server.sendDM(otherUser()?.id, JSON.stringify({ candidate }));
+		this.pc.onicecandidate = ({ candidate }) => sendMessage({ candidate })
+
+		this.pc.onsignalingstatechange = () => {
+			console.log(`RTCPeerConnection's signalingState changed: ${this.pc.signalingState}`)
+		}
+
+		console.log('PeerConnection: constructor finised!', this)
+	}
+
+	//https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation
+	//https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addTrack#adding_tracks_to_multiple_streams
+	async startCall() {
+		try {
+			this.localStream = await navigator.mediaDevices.getUserMedia(this.constraints);
+			if (this.localStream) {
+				// Push tracks from local stream to peer connection
+				this.localStream?.getTracks().forEach((track) => {
+					console.log(`adding ${track.muted ? "muted" : "un-muted"} local ${track.kind} track to peer connection:`, track.label)
+					this.localRTCRtpSenders.push(this.pc?.addTrack(track, this.localStream))
+					this.logTrackEvents(track, 'local');
+				});
+
+				this.onConnect(this.localStream, this.remoteStream)
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	}
+
+	endCall() {
+		console.groupCollapsed('end call...')
+
+		this.loggingAbortControllers.forEach(ac => {
+			console.log('aborting track logging event listeners!')
+			ac.abort()
+		})
+
+		this.localRTCRtpSenders.forEach(t => {
+			this.pc.removeTrack(t)
+			console.log('removed local RTCRtpSender', t)
+		})
+
+		this.localStream?.getTracks().forEach((track) => {
+			track.stop()
+			this.localStream.removeTrack(track)
+			console.log(`stopped ${track.muted ? "muted" : "un-muted"} local ${track.kind} track:`, track.label)
+		})
+
+		try {
+			//https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/close
+			this.pc?.close();
+		} catch (err) {
+			console.error(err)
+		}
+
+		this.onDisconnect()
+
+		console.groupEnd()
+	}
+
+	logTrackEvents(track: MediaStreamTrack, label: 'local' | 'remote') {
+		const ac = new AbortController()
+		this.loggingAbortControllers.push(ac)
+		track.addEventListener('ended', () => console.log(`ENDED: ${label} ${track.kind}: ${track.label}`), { signal: ac.signal })
+		track.addEventListener('mute', () => console.log(`MUTE: ${label} ${track.kind}: ${track.label}`), { signal: ac.signal })
+		track.addEventListener('unmute', () => console.log(`UNMUTE: ${label} ${track.kind}: ${track.label}`), { signal: ac.signal })
+	}
+
+	async handleMessage({ description, candidate }) {
+		try {
+
+			if (this.pc.signalingState === "closed") {
+				console.warn(`RTCPeerConnection's signalingState is 'closed'... retrying...`);
+				await new Promise((resolve) => setTimeout(() => resolve(''), 1)); //just give it a tick then try again...
+			}
+
+			if (description) {
+				const offerCollision = description.type === "offer"
+					&& (this.makingOffer || this.pc.signalingState !== "stable");
+
+				this.ignoreOffer = this.polite && offerCollision;
+				if (this.ignoreOffer) {
+					return;
+				}
+
+				if (this.pc.signalingState === "closed") {
+					console.error(`Ignoring offer description because The RTCPeerConnection's signalingState is 'closed'`, description);
+					return;
+				}
+
+				await this.pc.setRemoteDescription(description);
+				if (description.type === "offer") {
+					await this.pc.setLocalDescription();
+					// server.sendDM(otherUser()?.id, JSON.stringify({ description: this.pc.localDescription }));
+					this.sendMessage({ description: this.pc.localDescription })
+				}
+			} else if (candidate) {
+				try {
+					if (this.pc.signalingState === "closed") {
+						console.error(`Ignoring ice candidate because The RTCPeerConnection's signalingState is 'closed'`, candidate);
+						return;
+					}
+					await this.pc.addIceCandidate(candidate);
+				} catch (err) {
+					if (!this.ignoreOffer) {
+						throw err;
+					}
+				}
+			}
+		} catch (err) {
+			console.warn(err);
+		}
+	}
+}
+
 export default function VideoCall(props: { room: Room, user: Connection, connections: Connection[] }) {
 
 	if (!props.user) {
@@ -21,230 +242,57 @@ export default function VideoCall(props: { room: Room, user: Connection, connect
 	// 	console.log('trackStore(rooms)', r, isRoomOwner ? 'owner' : 'guest')
 	// });
 
-	const servers: RTCConfiguration = {
-		iceServers: [
-			// {
-			// 	urls: [
-			// 		'stun:stun1.l.google.com:19302',
-			// 		'stun:stun2.l.google.com:19302'
-			// 	],
-			// },
-			{
-				urls: "stun:stun.relay.metered.ca:80",
-			},
-			{
-				urls: "turn:global.relay.metered.ca:80",
-				username: "20cd52d0dc022700b2755c26",
-				credential: "MNMabfdDEZeLlOFU",
-			},
-			{
-				urls: "turn:global.relay.metered.ca:80?transport=tcp",
-				username: "20cd52d0dc022700b2755c26",
-				credential: "MNMabfdDEZeLlOFU",
-			},
-			{
-				urls: "turn:global.relay.metered.ca:443",
-				username: "20cd52d0dc022700b2755c26",
-				credential: "MNMabfdDEZeLlOFU",
-			},
-			{
-				urls: "turns:global.relay.metered.ca:443?transport=tcp",
-				username: "20cd52d0dc022700b2755c26",
-				credential: "MNMabfdDEZeLlOFU",
-			},
-		],
-		iceCandidatePoolSize: 10,
-	};
-
-
-	let localStream: MediaStream | undefined
-	let remoteStream = new MediaStream();
 
 	let webcamVideo: HTMLVideoElement
 	let remoteVideo: HTMLVideoElement
 
-	const constraints = { audio: true, video: true }
-
-	const pc = new RTCPeerConnection(servers);
-	console.log("------------------- Created Peer Connectin! -------------------")
-	//TODO: create peer connection for each other user in the room!!!!
-
-
-	function logTrackEvents(track: MediaStreamTrack, label: 'local' | 'remote') {
-		const ac = new AbortController()
-		loggingAbortControllers.push(ac)
-		track.addEventListener('ended', () => console.log(`ENDED: ${label} ${track.kind}: ${track.label}`), { signal: ac.signal })
-		track.addEventListener('mute', () => console.log(`MUTE: ${label} ${track.kind}: ${track.label}`), { signal: ac.signal })
-		track.addEventListener('unmute', () => console.log(`UNMUTE: ${label} ${track.kind}: ${track.label}`), { signal: ac.signal })
-	}
-
-	const loggingAbortControllers: AbortController[] = []
-	const localRTCRtpSenders: RTCRtpSender[] = []
-
-	function endCall() {
-		console.groupCollapsed('end call...')
-
-		loggingAbortControllers.forEach(ac => {
-			console.log('aborting track logging event listeners!')
-			ac.abort()
-		})
-
-		localRTCRtpSenders.forEach(t => {
-			pc.removeTrack(t)
-			console.log('removed local RTCRtpSender', t)
-		})
-
-		localStream?.getTracks().forEach((track) => {
-			track.stop()
-			localStream.removeTrack(track)
-			console.log(`stopped ${track.muted ? "muted" : "un-muted"} local ${track.kind} track:`, track.label)
-		})
-
-
-		try {
-			//https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/close
-			pc?.close();
-		} catch (err) {
-			console.error(err)
-		}
-
-		const roomId = props.user.roomId
-		if (roomId)
-			server.exitRoom(roomId)
-
-		console.groupEnd()
-	}
-
-	//https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Perfect_negotiation
-	//https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addTrack#adding_tracks_to_multiple_streams
-	async function startCall() {
-		try {
-			localStream = await navigator.mediaDevices.getUserMedia(constraints);
-			if (localStream) {
-				// Push tracks from local stream to peer connection
-				localStream?.getTracks().forEach((track) => {
-					console.log(`adding ${track.muted ? "muted" : "un-muted"} local ${track.kind} track to peer connection:`, track.label)
-					localRTCRtpSenders.push(pc?.addTrack(track, localStream))
-					logTrackEvents(track, 'local');
-				});
-
-				if (webcamVideo) {
-					webcamVideo.srcObject = localStream;
-					webcamVideo.muted = true;
-				}
-
-				if (remoteVideo) {
-					remoteVideo.srcObject = remoteStream;
-				}
+	const pm = new PeerConnection({
+		polite: props.user?.id === props.room?.ownerId,
+		sendMessage: (message: {}) => {
+			return server.sendDM(otherUser()?.id, JSON.stringify(message))
+		},
+		onConnect: (localStream: MediaStream, remoteStream: MediaStream) => {
+			if (webcamVideo) {
+				webcamVideo.srcObject = localStream;
+				webcamVideo.muted = true;
 			}
-		} catch (err) {
-			console.error(err);
-		}
-	}
 
-	// Pull tracks from remote stream, add to video stream
-	pc.ontrack = (event) => {
-		const track = event.track
-		console.log(`got ${event.type}: ${track.muted ? "muted" : "un-muted"} ${track.kind} from peer connection`, track.label)
-		track.addEventListener('end', () => remoteStream.removeTrack(track))
-		// track.addEventListener('mute', () => remoteStream.removeTrack(track))
-		track.addEventListener('unmute', () => remoteStream.addTrack(track))
-		//remoteStream.addTrack(track);
-		logTrackEvents(track, 'remote');
-	};
-
-	const [userOwnsRoom, setUserOwnsRoom] = createSignal(false)
-	const [polite, setPolite] = createSignal(false)
-
-	let makingOffer = false;
-	pc.onnegotiationneeded = async () => {
-		try {
-			makingOffer = true;
-			await pc.setLocalDescription();
-			await server.sendDM(otherUser()?.id, JSON.stringify({ description: pc.localDescription }))
-		} catch (err) {
-			console.error(err);
-		} finally {
-			makingOffer = false;
-		}
-	};
-
-	pc.oniceconnectionstatechange = () => {
-		console.log('oniceconnectionstatechange', pc.iceConnectionState, pc.signalingState)
-		if (pc.iceConnectionState === "failed") {
-			pc.restartIce();
-		}
-
-		if (pc.iceConnectionState === 'disconnected') {
+			if (remoteVideo) {
+				remoteVideo.srcObject = remoteStream;
+			}
+		},
+		onDisconnect: () => {
 			remoteVideo.srcObject = null
-			console.log(remoteStream)
-		}
-	};
 
-	pc.onicecandidate = ({ candidate }) => server.sendDM(otherUser()?.id, JSON.stringify({ candidate }));
-
-	pc.onsignalingstatechange = () => {
-		console.log(`RTCPeerConnection's signalingState changed: ${pc.signalingState}`)
-	}
-
-	let ignoreOffer = false;
-	server.onDM(async dm => {
-		try {
-			const { description, candidate } = JSON.parse(dm.message)
-			console.log(`DM from: ${dm.senderId}`, { description, candidate })
-
-			if (pc.signalingState === "closed") {
-				console.warn(`RTCPeerConnection's signalingState is 'closed'... retrying...`)
-				await new Promise((resolve) => setTimeout(() => resolve(''), 1)) //just give it a tick then try again...
-			}
-
-			if (description) {
-				const offerCollision = description.type === "offer"
-					&& (makingOffer || pc.signalingState !== "stable");
-
-				ignoreOffer = !polite() && offerCollision;
-				if (ignoreOffer) {
-					return;
-				}
-
-				if (pc.signalingState === "closed") {
-					console.error(`Ignoring offer description because The RTCPeerConnection's signalingState is 'closed'`, description)
-					return
-				}
-
-				await pc.setRemoteDescription(description);
-				if (description.type === "offer") {
-					await pc.setLocalDescription();
-					server.sendDM(otherUser()?.id, JSON.stringify({ description: pc.localDescription }));
-				}
-			} else if (candidate) {
-				try {
-					if (pc.signalingState === "closed") {
-						console.error(`Ignoring ice candidate because The RTCPeerConnection's signalingState is 'closed'`, candidate)
-						return
-					}
-					await pc.addIceCandidate(candidate);
-				} catch (err) {
-					if (!ignoreOffer) {
-						throw err;
-					}
-				}
-			}
-		} catch (err) {
-			console.warn(err);
+			// const roomId = props.user.roomId
+			// if (roomId)
+			// 	server.exitRoom(roomId)
 		}
 	})
+	server.onDM((dm) => {
+		const { description, candidate } = JSON.parse(dm.message);
+		console.log(`DM from: ${dm.senderId}`, { description, candidate });
+
+		//TODO: route this message to the correct peer connection for the sender
+
+		pm.handleMessage({ description, candidate })
+	})
+
+	console.log("------------------- Created Peer Connection! -------------------")
+	//TODO: create peer connection for each other user in the room!!!!
+
 
 	const otherUser = () => props.connections[0]
 
 	onCleanup(() => {
 		console.log('video call cleanup ... other side ended call!')
-		endCall()
+		pm.endCall()
 	})
 
 	onMount(() => {
 		if (otherUser()) {
-			startCall()
+			console.log('OTHER USER exists! Start the call!')
+			pm.startCall()
 		} else {
 			console.log(`it's quiet... waiting for someone else to join...`)
 		}
@@ -252,21 +300,10 @@ export default function VideoCall(props: { room: Room, user: Connection, connect
 
 	createEffect(() => {
 		console.log("EFFECT", props.connections)
-		const isOwner = props.user?.id === props.room?.ownerId
-		setUserOwnsRoom(isOwner)
-		setPolite(!isOwner)
-
-		console.log({ user: props.user, room: props.room })
-		console.log('set userOwnsRoom', userOwnsRoom())
-		console.log('set polite', polite())
-
 		if (props.connections.length > 0) {
 			console.log('someone joined!!!')
-
-
-			startCall()
+			pm.startCall()
 		}
-
 	})
 
 	return <div class="video-call">
@@ -276,8 +313,6 @@ export default function VideoCall(props: { room: Room, user: Connection, connect
 		<div class="video-container">
 			<video class="remote" ref={remoteVideo} autoplay playsinline></video>
 		</div>
-		{/* <div>Owner: {userOwnsRoom() ? "owner" : "guest"}</div>
-		<div>Polite: {polite() ? "yes" : "no"}</div> */}
 		<div class="connections">
 			<Participant con={props.user} ownsRoom={true} />
 			<For each={props.connections}>
