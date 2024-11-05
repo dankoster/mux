5cd8ee0cb49246654d76909ba94709aaa4e37b00
Dan Koster <koster.dan@gmail.com>
2024-11-05 12:09:41 -0800

    move github svg

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import "./index.css"
 import "./main.css"
 
 import { createEffect, createMemo, createSignal, For, Show } from "solid-js";
@@ -9,6 +8,7 @@ import ConnectionsGraph from "./Connections";
 import { trackStore } from "@solid-primitives/deep";
 
 import type { Connection, DM } from "../server/types"
+import { GitHubSvg } from "./GitHubSvg";
 
 type CallState = "no_call" | "server_wait" | "server_error" | "call_ready" | "call_connected"
 
@@ -107,7 +107,9 @@ const App = () => {
 
 	const [selectedDm, setSlectedDm] = createSignal<Connection>()
 
+
 	return <>
+		<meta name="theme-color" content="#1f0e3c"></meta>
 		<Show when={!server.serverOnline()}>
 			<div class="offlineMessage">connecting...</div>
 		</Show>
@@ -123,24 +125,18 @@ const App = () => {
 				<div class="user">
 					<Show when={!server.self()?.identity}>
 						<div class="color-picker">
-							<input 
+							<input
 								class="color-input"
 								type="color"
 								oninput={(e) => e.target.parentElement.style.backgroundColor = e.target.value}
 								onchange={(e) => server.setColor(e.target.value)}
 								value={server.self()?.color ?? 'transparent'} />
-
 						</div>
 					</Show>
 					<Show when={!server.self()?.identity}>
 						<a class="room-button" href={server.githubAuthUrl()?.toString()}>
-							<svg height="16" width="16" viewBox="0 0 16 16" version="1.1" aria-hidden="true">
-								<path
-									d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-									fill-rule="evenodd"
-									fill="currentColor"></path>
-							</svg>
-							login</a>
+							<GitHubSvg />login
+						</a>
 					</Show>
 					<Show when={server.self()?.identity}>
 						<div class="avatar button" onclick={() => server.becomeAnonymous()}>
@@ -223,7 +219,7 @@ const App = () => {
 								</Show>
 								{/* {c.identity?.id ? `[${c.identity.id}] ` : `[${c.id?.substring(c.id.length - 4)}] `} */}
 								{c.identity?.name || `guest`} ({c.kind})
-								<Show when={hasPendingFriendRequest(c)}><span>pending friend request...</span></Show>
+								<Show when={hasPendingFriendRequest(c)}><span>👋</span></Show>
 								<Show when={canFriendRequest(c)}>
 									<button onClick={() => server.sendFriendRequest(c)}>friend request</button>
 								</Show>
@@ -255,4 +251,4 @@ const App = () => {
 };
 
 
-render(() => <App />, document.body);
+render(() => <App />, document.body)
