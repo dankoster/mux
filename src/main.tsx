84229eb9295bdfcf38d42e71bfbbd33ee86ed0ee
Dan Koster <koster.dan@gmail.com>
2024-12-17 00:44:39 -0800

    more layout tinkering

--- src/main.tsx
+++ src/main.tsx
@@ -92,6 +92,12 @@ function App() {
 
 
 			<div class="toolbar">
+				<div class="stats">
+					<h2 class="logo">⨳</h2>
+					<div class="userCount"><b>{server.stats()?.online ?? "?"}</b> online 👀</div>
+					<div class="userCount"><b>{server.stats()?.offline ?? "?"}</b> offline 😴</div>
+				</div>
+
 				<div class="user">
 
 					{/* <div class="color-picker">
