0ed58b121af5f94f5d0a04fbf4d0638424b28e41
Dan Koster <koster.dan@gmail.com>
2025-10-29 00:58:57 -0700

    timeout log entries after a few sec

--- src/uiLog.ts
+++ src/uiLog.ts
@@ -3,12 +3,27 @@ export function uiLog(value: string) {
 	let logElement = document.getElementById('logger')
 
 	if (!logElement) {
-		logElement = document.createElement('pre')
+		logElement = document.createElement('div')
 		logElement.id = 'logger'
 		logElement.style.position = 'absolute'
 		logElement.style.backdropFilter = "blur(10px)"
+		logElement.style.maxHeight = '10rem'
+		logElement.style.overflow = 'hidden'
+		logElement.style.opacity = '0.7'
+
 		document.body.appendChild(logElement)
 	}
 
-	logElement.textContent += value + "\n"
+	if(logElement.children.length > 10)
+		logElement.children[0].remove()
+
+	const logEntry = document.createElement('pre')
+	logEntry.style.margin = "0"
+	logEntry.textContent = value
+	logElement.appendChild(logEntry)
+	logEntry.scrollIntoView() //{behavior: "smooth"})
+
+	setTimeout(() => {
+		logEntry.remove()
+	}, 3000);
 }
