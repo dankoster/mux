8cb90a9154ec134e94c43a8c2abff1aa9bedc877
Dan Koster <koster.dan@gmail.com>
2026-01-01 18:18:25 -0800

    update imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { Show } from "solid-js";
 import { render } from "solid-js/web";
 import { Planet } from "./planet/planet";
 import Settings, { settingsVisible } from "./Settings";
-import VideoCall, * as videoCall from "./VideoCall";
+import VideoCall from "./VideoCall";
 import * as server from "./data/data";
 
 import "./main.css"
