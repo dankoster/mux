fc3051836ccd8cd77bb3c04ee97e4dcc26fdfb1d
Dan Koster <koster.dan@gmail.com>
2026-08-10 23:49:00 -0700

    add log label

--- src/planet/snapshot.ts
+++ src/planet/snapshot.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { addLights } from './lighting';
+import { addLights } from './lighting'
 
 //webgl limits us to 8 concurrent renering contexts! https://threejs.org/manual/#en/multiple-scenes
 //so use a shared module level renderer here
@@ -20,12 +20,12 @@ export async function RenderSnapshotToUrl(object: THREE.Group | THREE.Object3D) 
 	const snapCamera = new THREE.PerspectiveCamera(50, 1)
 	//TODO calculate the camera z so the entire object is in the frustum
 	snapCamera.position.z = 4
-	snapCamera.updateProjectionMatrix();	
+	snapCamera.updateProjectionMatrix()
 	
 	//look at the center of the object
-	const centerOfMesh = new THREE.Vector3();
+	const centerOfMesh = new THREE.Vector3()
 	var boundingBox = new THREE.Box3()
-	boundingBox.setFromObject(object);
+	boundingBox.setFromObject(object)
 	boundingBox.getCenter(centerOfMesh)
 	snapCamera.lookAt(centerOfMesh)
 	
@@ -37,8 +37,8 @@ export async function RenderSnapshotToUrl(object: THREE.Group | THREE.Object3D) 
 			//https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob
 			snapRenderer.domElement.toBlob((blob) => {
 				if(!blob) throw new Error(`blob is ${blob}`)
-				const url = URL.createObjectURL(blob);
-				console.log(url)
+				const url = URL.createObjectURL(blob)
+				console.log('snapshot created!', url)
 				resolve(url)
 			})
 		} catch (ex) {
