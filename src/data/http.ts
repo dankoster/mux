d4e29b096e78f70451bae2f3b3078e079b296962
Dan Koster <koster.dan@gmail.com>
2026-08-11 20:41:02 -0700

    use headers constructor

--- src/data/http.ts
+++ src/data/http.ts
@@ -26,19 +26,18 @@ export const apiRoute: { [Property in ApiRoute]: Property } = {
 };
 
 export async function GET(route: ApiRoute, subRoute?: string) {
-	const headers = {};
-	headers[AUTH_TOKEN_HEADER_NAME] = pk();
+	const token = pk()
+	const headers = new Headers()
+	if (token) headers.append(AUTH_TOKEN_HEADER_NAME, token)
 	const url = [API_URI, route, subRoute].filter(s => s).join('/')
-	return await fetch(url, {
-		method: "GET",
-		headers
-	});
+	return await fetch(url, { method: "GET", headers });
 }
 
 type PostOptions = { subRoute?: string, body?: string, authToken?: string }
 export async function POST(route: ApiRoute, options?: PostOptions) {
-	const headers = {};
-	headers[AUTH_TOKEN_HEADER_NAME] = options?.authToken ?? pk();
+	const token = options?.authToken ?? pk()
+	const headers = new Headers()
+	if (token) headers.append(AUTH_TOKEN_HEADER_NAME, token)
 	if (!route) throw new Error(`invalid route: ${route}`)
 	const url = [API_URI, route, options?.subRoute].filter(s => s).join('/')
 	const result = await fetch(url, {
@@ -51,8 +50,9 @@ export async function POST(route: ApiRoute, options?: PostOptions) {
 }
 
 export async function DELETE(route: ApiRoute, subRoute: string) {
-	const headers = {};
-	headers[AUTH_TOKEN_HEADER_NAME] = pk();
+	const token = pk()
+	const headers = new Headers()
+	if (token) headers.append(AUTH_TOKEN_HEADER_NAME, token)
 	const url = [API_URI, route, subRoute].join('/')
 	return await fetch(url, { method: "DELETE", headers });
 }
