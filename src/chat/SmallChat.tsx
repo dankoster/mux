a9cacda9b175e2581d1e0d707c81783d6b35fb56
Dan Koster <koster.dan@gmail.com>
2026-08-16 21:37:37 -0700

    use onNewMessageForConnection

--- src/chat/SmallChat.tsx
+++ src/chat/SmallChat.tsx
@@ -1,42 +1,63 @@
-import { createSignal, For, onCleanup, onMount, Show } from "solid-js"
+import { Accessor, createSignal, For, onCleanup, onMount, Show } from "solid-js"
 import { Connection, DM } from "../../server/types";
 
-import * as directMessages from "../data/directMessages";
 import * as server from "../data/data";
+import * as directMessages from "../data/directMessages";
 
 import "./Chat.css"
 import { uiLog } from "../uiLog";
 
-export default function SmallChat(props: { connection: Connection }) {
+export default function SmallChat(props: { connection: Accessor<Connection | undefined> }) {
+
+	console.log('smallchat', props.connection())
+	if (!props.connection) return <>no connection!</>
 
 	let messagesRef: HTMLDivElement
+	let inputRef: HTMLInputElement
+	let onNewMessage_AbortController: AbortController
 
 	const [dmList, setDmList] = createSignal<DM[]>([], { equals: false })
 
-	directMessages.onNewMessage((dm: DM) => {
-		setDmList([...dmList(), dm])
-		scrollToLatest()
-	})
-
-	const scrollToLatest = () => messagesRef.lastElementChild?.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" })
+	const scrollToLatest = (behavior: ScrollBehavior = "auto") => messagesRef!.lastElementChild?.scrollIntoView({ behavior: behavior, block: "center", inline: "nearest" })
 
 	onMount(async () => {
-		let history = await directMessages.getLatestHistory(props.connection.id, 60)
+		let history = await directMessages.getLatestHistory(props.connection()!?.id, 60)
 		setDmList(history)
 		scrollToLatest()
+		inputRef!?.focus()
+
+		if (onNewMessage_AbortController) throw new Error('abort controller must be null')
+
+		//not sure I like this pattern
+		onNewMessage_AbortController = directMessages.onNewMessageForConnection(props.connection()!, (dm: DM) => {
+
+			console.log('SmallChat for', props.connection()!.id, 'got message from', dm.fromName, 'to', dm.toId)
+
+			if (![dm.fromId, dm.toId].includes(props.connection()!.id))
+				return //this message is for a different connection 
+
+			setDmList([...dmList(), dm])
+			scrollToLatest("smooth")
+		})
 	})
 
 	onCleanup(() => {
-		//todo cleanup
+		onNewMessage_AbortController.abort()
 	})
 
 	async function sendDm(con: Connection, message: string) {
 		try {
+			if (!con) throw new Error(`con is ${con}`)
+			if (!con.publicKey) throw new Error(`con.publicKey is ${con.publicKey}`)
+
 			const self = server.self();
+			if (!self) throw new Error(`self is ${self}`)
+
 			const dm: DM = {
 				toId: con.id,
 				fromId: self.id,
 				fromName: self.identity?.name,
+				timestamp: Date.now(),
 				message,
 				kind: "text"
 			}
@@ -46,9 +67,9 @@ export default function SmallChat(props: { connection: Connection }) {
 			//scroll last message into view
 			const dmElements = Array.from(document.getElementsByClassName('dm'));
 			dmElements[dmElements.length - 1]?.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
-			scrollToLatest()
+			scrollToLatest("smooth")
 		} catch (err) {
-			uiLog(err.message)
+			uiLog((err as any)?.message ?? 'error sending dm')
 			console.error(err)
 		}
 	}
@@ -64,21 +85,22 @@ export default function SmallChat(props: { connection: Connection }) {
 
 
 	return <div class="small-chat">
-		<div class="messages" ref={messagesRef}>
+		<div class="messages" ref={messagesRef!}>
 			<For each={dmList()}>
 				{dm => <div class="message" classList={{
-					'incoming': dm.fromId == props.connection.id,
-					'outgoing': dm.toId == props.connection.id
+					'incoming': dm.fromId == props.connection()?.id,
+					'outgoing': dm.toId == props.connection()?.id
 				}}>
 					{dm.message as string}
 				</div>}
 			</For>
 		</div>
 		<input
+			ref={inputRef!}
 			class='dm-send-input'
 			type="text"
-			placeholder={`To ${props.connection?.identity?.name}...`}
-			onKeyDown={(e) => onMessageKeyDown(e, props.connection)}>
+			placeholder={`To ${props.connection()?.identity?.name}...`}
+			onKeyDown={(e) => onMessageKeyDown(e, props.connection()!)}>
 		</input>
 	</div>
 }
